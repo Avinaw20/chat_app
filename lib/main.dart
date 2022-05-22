2ce173546d4a9f6c159a9cfd2f413d0786f2f8fb
avinaw20 <avinaw20@gmail.com>
2022-05-22 15:13:55 +0530

    https://github.com/Avinaw20/chat_app.git

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:chat_app/controller/login_controller.dart';
+import 'package:chat_app/login_page.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter_Code_Sample';
-
+  //This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      ' Messasing App',
-      style: optionStyle,
-    ),
-    Text(
-      ' Single Chat',
-      style: optionStyle,
-    ),
-    Text(
-      ' Group Chat',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Messaging App'),
+  Widget build(BuildContext context){
+    return MultiProvider(
+        providers:[
+          ChangeNotifierProvider(create: (context) => LoginController(),
+          )
+        ] ,
+    child: MaterialApp(
+      title: 'Messaging App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
-            label: 'Single Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group_add_outlined),
-            label: 'Group Chat',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+      home: LoginPage(),
+      )
+
+    ),
     );
   }
 }
